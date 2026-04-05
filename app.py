137c03ebb9ca7f82a9a319049576a356055c49c7
teacherandresrueda <teacherandresrueda@gmail.com>
2026-04-05 17:21:21 -0600

    Add files via upload

---
+++ app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+from melate_sistema_total import 
+
+st.title(🎯 Melate AI - Sistema Inteligente)
+
+st.write(Generador de números basado en patrones)
+
+if st.button(Generar combinación)
+    resultado = generar_numeros()
+    st.success(fTu combinación {resultado})
